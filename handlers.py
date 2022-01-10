417aee169051eedffba352d336a5904e6a122b8d
Ju4ara5 <90102282+Ju4ara5@users.noreply.github.com>
2022-01-10 15:30:15 +0200

    echo bot

--- handlers.py
+++ handlers.py
@@ -8,6 +8,7 @@ from config import admin_id
 async def send_to_admin(dp):
     await bot.send_message(chat_id=admin_id, text='Бот запущен.')
 
+
 @dp.message_handler()
 async def echo(message: Message):
     text = f'Привет, ты написал: {message.text}'
